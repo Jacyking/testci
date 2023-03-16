d6ae45573c126609f9af67f383636017f44075ba
Jacyking <43704572+Jacyking@users.noreply.github.com>
2023-03-16 15:01:22 +0800

    Update test_testci.cpp

--- tests/test_testci.cpp
+++ tests/test_testci.cpp
@@ -1,4 +1,9 @@
 #include "doctest.h"
 #include "test.hpp"
 
-TEST_CASE("testci") { CHECK(testci() == 1); }
+TEST_CASE("test case 1") { CHECK(testci() == 1); }
+
+TEST_CASE("test case 2") { 
+  auto result = testci();
+  CHECK(result == 1); 
+}
